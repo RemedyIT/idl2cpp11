5eceb14680f4d745c9bbf527600318b88e380993
Martin Corino <mcorino@remedy.nl>
2011-06-09 12:28:43 +0000

    first example
    git-svn-id: file:///srv/svn/repos/pub_idl2c++0x/trunk@2 338aa3ae-290a-4059-bdbc-492bcc190b6e

---
+++ examples/Hello/client.cpp
@@ -0,0 +1,62 @@
+/*--------------------------------------------------------------------
+ * client.cpp - CORBA C++0x client application
+ *
+ * Author: Martin Corino
+ *
+ * $Id$
+ *
+ * Copyright (c) Remedy IT Expertise BV
+ * Chamber of commerce Rotterdam nr.276339, The Netherlands
+ --------------------------------------------------------------------*/
+
+#include "TestC.h"
+#include "tao/0x/orb.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  try
+    {
+      CORBA::ORB _orb = CORBA::ORB_init (argc, argv);
+
+      if (_orb == nullptr)
+      {
+        std::cerr << "ERROR: CORBA::ORB_init (argc, argv) returned null ORB." << std::endl;
+        exit (1);
+      }
+
+      CORBA::Object obj = _orb->string_to_object ("file://test.ior");
+
+      if (obj == nullptr)
+      {
+        std::cerr << "ERROR: string_to_object(<ior>) returned null reference." << std::endl;
+        exit (1);
+      }
+
+      std::cout << "retrieved object reference" << std::endl;
+
+      Test::Hello hello = Test::Hello::narrow (obj);
+
+      if (!hello)
+      {
+        std::cerr << "ERROR: Test::Hello::narrow (obj) returned null object." << std::endl;
+        exit (1);
+      }
+
+      std::cout << "narrowed Hello interface" << std::endl;
+
+      std::cout << "hello->get_string () returned " << hello->get_string () << std::endl;
+
+      std::cout << "shutting down...";
+
+      hello->shutdown ();
+
+      std::cout << std::endl;
+    }
+  catch (const std::exception& e)
+    {
+      std::cerr << "exception caught: " << e.what() << std::endl;
+    }
+  return 0;
+}
