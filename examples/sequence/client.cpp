34d148280cd5ea112d7d5948754df53b50bfa246
Johnny Willemsen <jwillemsen@remedy.nl>
2012-02-08 20:56:55 +0100

    Made sequence client compiling

--- examples/sequence/client.cpp
+++ examples/sequence/client.cpp
@@ -9,9 +9,7 @@
  * Chamber of commerce Rotterdam nr.276339, The Netherlands
  --------------------------------------------------------------------*/
 
-#include "TestC.h"
-#include "tao/0x/orb.h"
-
+#include "testC.h"
 #include <iostream>
 
 template <typename T>
@@ -65,7 +63,7 @@ int main(int argc, char* argv[])
 {
   try
     {
-      CORBA::ORB _orb = CORBA::ORB_init (argc, argv);
+      CORBA::object_reference<CORBA::ORB> _orb = CORBA::ORB_init (argc, argv);
 
       if (_orb == nullptr)
       {
@@ -73,7 +71,7 @@ int main(int argc, char* argv[])
         exit (1);
       }
 
-      CORBA::Object obj = _orb->string_to_object ("file://test.ior");
+      CORBA::object_reference<CORBA::Object> obj = _orb->string_to_object ("file://test.ior");
 
       if (obj == nullptr)
       {
@@ -83,7 +81,7 @@ int main(int argc, char* argv[])
 
       std::cout << "retrieved object reference" << std::endl;
 
-      Test::Foo foo = Test::Foo::narrow (obj);
+      CORBA::object_reference<Test::Foo> foo = Test::Foo::narrow (obj);
 
       if (!foo)
       {
