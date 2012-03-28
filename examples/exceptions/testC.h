c3f20fe60e4cd0bcf834595ed72848f45afa6088
Marcel Smit <msmit@remedy.nl>
2012-03-28 09:44:21 +0200

    Fuzz: removed whitespaces.

--- examples/exceptions/testC.h
+++ examples/exceptions/testC.h
@@ -234,7 +234,7 @@ inline ::Test::FooEx& Test::FooEx::operator= (::Test::FooEx&& x)
   }
   return *this;
 }
-  
+
 
 // generated from c++/cli_inl/except_inl.erb
 inline Test::BooEx::BooEx ()
@@ -270,7 +270,7 @@ inline ::Test::BooEx& Test::BooEx::operator= (::Test::BooEx&& x)
   }
   return *this;
 }
-  
+
 
 // generated from StubHeaderStdWriter#pre_visit
 namespace std {
