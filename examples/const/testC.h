8e1d4be5f36ef8c1fc9063504938819f17c78902
Johnny Willemsen <jwillemsen@remedy.nl>
2014-03-31 14:06:27 +0200

    Enable ostream insertion by default and regenerated       all     * examples/array/array_p.mpc:     * examples/array/testC.h:     * examples/const/const_p.mpc:     * examples/const/testC.h:     * examples/deployment/deployment_p.mpc:     * examples/deployment/testC.h:     * examples/enum/enum_p.mpc:     * examples/enum/testC.h:     * examples/exceptions/exceptions_p.mpc:     * examples/exceptions/testC.h:     * examples/exceptions/testS.h:     * examples/hello/hello.mpc:     * examples/hello/testC.h:     * examples/hello/testS.h:     * examples/hello_auto/hello_auto.mpc:     * examples/hello_auto/testC.h:     * examples/hello_auto/testS.h:     * examples/local/local_p.mpc:     * examples/local/testC.h:     * examples/local/testS.h:     * examples/objrefarg/objrefarg_p.mpc:     * examples/objrefarg/testC.h:     * examples/radartrack/radartrack_p.mpc:     * examples/radartrack/testC.h:     * examples/sequence/sequence_p.mpc:     * examples/sequence/testC.h:     * examples/shapes/shapes_p.mpc:     * examples/shapes/testC.h:     * examples/shapes/testS.h:     * examples/struct/struct_p.mpc:     * examples/struct/testC.h:     * examples/union/testC.h:     * examples/union/union_p.mpc:     * examples/valuetype/testC.h:     * examples/valuetype/testS.h:     * examples/valuetype/valuetype_p.mpc:

--- examples/const/testC.h
+++ examples/const/testC.h
@@ -8,8 +8,8 @@
  *        http://www.remedy.nl \ http://www.theaceorb.nl
  */
 
-#ifndef __RIDL_TESTC_H_BAIABGJH_INCLUDED__
-#define __RIDL_TESTC_H_BAIABGJH_INCLUDED__
+#ifndef __RIDL_TESTC_H_CJCIJJIG_INCLUDED__
+#define __RIDL_TESTC_H_CJCIJJIG_INCLUDED__
 
 
 #include /**/ "ace/pre.h"
@@ -438,6 +438,6 @@ inline std::ostream& operator<< (
 
 #include /**/ "ace/post.h"
 
-#endif /* __RIDL_TESTC_H_BAIABGJH_INCLUDED__ */
+#endif /* __RIDL_TESTC_H_CJCIJJIG_INCLUDED__ */
 
 // -*- END -*-
--- examples/exceptions/testS.h
+++ examples/exceptions/testS.h
@@ -8,8 +8,8 @@
  *        http://www.remedy.nl \ http://www.theaceorb.nl
  */
 
-#ifndef __RIDL_TESTS_H_IFEFDCBF_INCLUDED__
-#define __RIDL_TESTS_H_IFEFDCBF_INCLUDED__
+#ifndef __RIDL_TESTS_H_BGHJEIFH_INCLUDED__
+#define __RIDL_TESTS_H_BGHJEIFH_INCLUDED__
 
 #include "testC.h"
 
@@ -124,6 +124,6 @@ namespace TAOX11_NAMESPACE {
   }; // CORBA
 }; // TAOX11_NAMESPACE
 
-#endif /* __RIDL_TESTS_H_IFEFDCBF_INCLUDED__ */
+#endif /* __RIDL_TESTS_H_BGHJEIFH_INCLUDED__ */
 
 // -*- END -*-
