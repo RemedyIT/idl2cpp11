2a57e49e9d41ac88c1b532182ae120264b40403a
Johnny Willemsen <jwillemsen@remedy.nl>
2014-05-09 16:29:53 +0200

    Regenerated after the release of TAOX11 v1.3     * examples/array/testC.h:     * examples/const/testC.h:     * examples/deployment/testC.h:     * examples/enum/testC.h:     * examples/exceptions/testC.h:     * examples/exceptions/testS.h:     * examples/hello/testC.h:     * examples/hello/testS.h:     * examples/hello_auto/testC.h:     * examples/hello_auto/testS.h:     * examples/local/testC.h:     * examples/local/testS.h:     * examples/objrefarg/testC.h:     * examples/radartrack/testC.h:     * examples/sequence/testC.h:     * examples/shapes/testC.h:     * examples/shapes/testS.h:     * examples/struct/testC.h:     * examples/union/testC.h:     * examples/valuetype/testC.h:     * examples/valuetype/testS.h:

--- examples/hello/testS.h
+++ examples/hello/testS.h
@@ -8,8 +8,8 @@
  *        http://www.remedy.nl \ http://www.theaceorb.nl
  */
 
-#ifndef __RIDL_TESTS_H_FDBHIJII_INCLUDED__
-#define __RIDL_TESTS_H_FDBHIJII_INCLUDED__
+#ifndef __RIDL_TESTS_H_EGBAFFFH_INCLUDED__
+#define __RIDL_TESTS_H_EGBAFFFH_INCLUDED__
 
 #include "testC.h"
 
@@ -21,8 +21,8 @@
 
 #include "tao/x11/versionx11.h"
 
-#if TAOX11_MAJOR_VERSION != 1 || TAOX11_MINOR_VERSION != 3 || TAOX11_BETA_VERSION != 0
-#error This file was generated with another RIDL C++11 backend version (1.3.0). Please re-generate.
+#if TAOX11_MAJOR_VERSION != 1 || TAOX11_MINOR_VERSION != 4 || TAOX11_BETA_VERSION != 0
+#error This file was generated with another RIDL C++11 backend version (1.4.0). Please re-generate.
 #endif
 
 // generated from ServantHeaderWriter#enter_module
@@ -117,6 +117,6 @@ namespace TAOX11_NAMESPACE {
   }; // CORBA
 }; // TAOX11_NAMESPACE
 
-#endif /* __RIDL_TESTS_H_FDBHIJII_INCLUDED__ */
+#endif /* __RIDL_TESTS_H_EGBAFFFH_INCLUDED__ */
 
 // -*- END -*-
--- examples/radartrack/testC.h
+++ examples/radartrack/testC.h
@@ -8,8 +8,8 @@
  *        http://www.remedy.nl \ http://www.theaceorb.nl
  */
 
-#ifndef __RIDL_TESTC_H_HEGFACCH_INCLUDED__
-#define __RIDL_TESTC_H_HEGFACCH_INCLUDED__
+#ifndef __RIDL_TESTC_H_JHBDIDFF_INCLUDED__
+#define __RIDL_TESTC_H_JHBDIDFF_INCLUDED__
 
 
 #include /**/ "ace/pre.h"
@@ -23,8 +23,8 @@
 
 #include "tao/x11/versionx11.h"
 
-#if TAOX11_MAJOR_VERSION != 1 || TAOX11_MINOR_VERSION != 3 || TAOX11_BETA_VERSION != 0
-#error This file was generated with another RIDL C++11 backend version (1.3.0). Please re-generate.
+#if TAOX11_MAJOR_VERSION != 1 || TAOX11_MINOR_VERSION != 4 || TAOX11_BETA_VERSION != 0
+#error This file was generated with another RIDL C++11 backend version (1.4.0). Please re-generate.
 #endif
 
 using namespace TAOX11_NAMESPACE;
@@ -363,6 +363,6 @@ operator<< (
 
 #include /**/ "ace/post.h"
 
-#endif /* __RIDL_TESTC_H_HEGFACCH_INCLUDED__ */
+#endif /* __RIDL_TESTC_H_JHBDIDFF_INCLUDED__ */
 
 // -*- END -*-
