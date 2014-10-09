edd41f21185bfefbcaea7097435c5ca4f710eb82
Johnny Willemsen <jwillemsen@remedy.nl>
2014-10-09 12:50:38 +0200

    Regenerated with the latest ridlc version     * examples/array/testC.h:     * examples/const/testC.h:     * examples/deployment/testC.h:     * examples/enum/testC.h:     * examples/exceptions/testC.h:     * examples/exceptions/testS.h:     * examples/hello/testC.h:     * examples/hello/testS.h:     * examples/hello_auto/testC.h:     * examples/hello_auto/testS.h:     * examples/local/testC.h:     * examples/local/testS.h:     * examples/objrefarg/testC.h:     * examples/radartrack/testC.h:     * examples/sequence/testC.h:     * examples/shapes/testC.h:     * examples/shapes/testS.h:     * examples/struct/testC.h:     * examples/union/testC.h:     * examples/valuetype/testC.h:     * examples/valuetype/testS.h:

--- examples/local/testS.h
+++ examples/local/testS.h
@@ -8,8 +8,8 @@
  *        http://www.remedy.nl \ http://www.theaceorb.nl
  */
 
-#ifndef __RIDL_TESTS_H_DACJGGFJ_INCLUDED__
-#define __RIDL_TESTS_H_DACJGGFJ_INCLUDED__
+#ifndef __RIDL_TESTS_H_CFHDDFEH_INCLUDED__
+#define __RIDL_TESTS_H_CFHDDFEH_INCLUDED__
 
 #include "testC.h"
 
@@ -21,8 +21,8 @@
 
 #include "tao/x11/versionx11.h"
 
-#if TAOX11_MAJOR_VERSION != 1 || TAOX11_MINOR_VERSION != 3 || TAOX11_BETA_VERSION != 2
-#error This file was generated with another RIDL C++11 backend version (1.3.2). Please re-generate.
+#if TAOX11_MAJOR_VERSION != 1 || TAOX11_MINOR_VERSION != 4 || TAOX11_BETA_VERSION != 0
+#error This file was generated with another RIDL C++11 backend version (1.4.0). Please re-generate.
 #endif
 
 // generated from ServantHeaderWriter#enter_module
@@ -37,6 +37,6 @@ namespace TAOX11_NAMESPACE {
   } // namespace CORBA
 } // namespace TAOX11_NAMESPACE
 
-#endif /* __RIDL_TESTS_H_DACJGGFJ_INCLUDED__ */
+#endif /* __RIDL_TESTS_H_CFHDDFEH_INCLUDED__ */
 
 // -*- END -*-
