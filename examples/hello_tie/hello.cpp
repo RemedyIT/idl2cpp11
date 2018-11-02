053c375111216301c7c264e01e9700663ea86d32
Johnny Willemsen <jwillemsen@remedy.nl>
2018-11-02 18:18:59 +0100

    New Hello example using delegation (tie)

---
+++ examples/hello_tie/hello.cpp
@@ -0,0 +1,27 @@
+/*!
+ * @file   hello.cpp
+ * @author Johnny Willemsen <jwillemsen@remedy.nl>
+ *
+ * @brief  CORBA C++11 Test::Hello implementation source
+ *
+ * @copyright Copyright (c) Remedy IT Expertise BV
+ * Chamber of commerce Rotterdam nr.276339, The Netherlands
+ */
+#include "hello.h"
+
+Hello::Hello (IDL::traits<CORBA::ORB>::ref_type orb)
+  : orb_ (std::move(orb))
+{
+}
+
+std::string
+Hello::get_string ()
+{
+  return "Hello there!";
+}
+
+void
+Hello::shutdown ()
+{
+  this->orb_->shutdown (false);
+}
---
+++ examples/hello_tie/hello.h
@@ -0,0 +1,35 @@
+/*!
+ * @file   hello.h
+ * @author Johnny Willemsen <jwillemsen@remedy.nl>
+ *
+ * @brief  CORBA C++11 Test::Hello implementation header
+ *
+ * @copyright Copyright (c) Remedy IT Expertise BV
+ * Chamber of commerce Rotterdam nr.276339, The Netherlands
+ */
+#ifndef HELLO_H
+#define HELLO_H
+#include /**/ "ace/pre.h"
+
+#include "tao/x11/orb.h"
+
+/// Regular class used through delegation
+class Hello final
+{
+public:
+  /// Constructor
+  Hello (IDL::traits<CORBA::ORB>::ref_type orb);
+
+  // = The skeleton methods
+  virtual std::string get_string () override;
+
+  virtual void shutdown () override;
+
+private:
+  /// Use an ORB reference to convert strings to objects and shutdown
+  /// the application.
+  IDL::traits<CORBA::ORB>::ref_type orb_;
+};
+
+#include /**/ "ace/post.h"
+#endif /* HELLO_H */
