36735a47c9e88229f94ddf0ed301f2a544146f3b
Johnny Willemsen <jwillemsen@remedy.nl>
2022-12-08 09:20:48 +0100

    Regenerated using lates ridlc with union simplifications
        * examples/array/testC.h:
        * examples/const/testC.h:
        * examples/dds/testC.h:
        * examples/dds/testS.h:
        * examples/deployment/testC.h:
        * examples/enum/testC.h:
        * examples/exceptions/testC.h:
        * examples/exceptions/testS.h:
        * examples/hello/testC.h:
        * examples/hello/testS.h:
        * examples/hello_auto/testC.h:
        * examples/hello_auto/testS.h:
        * examples/local/testC.h:
        * examples/local/testS.h:
        * examples/objrefarg/testC.h:
        * examples/radartrack/testC.h:
        * examples/sequence/testC.h:
        * examples/struct/testC.h:
        * examples/union/testC.h:
        * examples/valuetype/testC.h:
        * examples/valuetype/testS.h:

--- examples/exceptions/testC.h
+++ examples/exceptions/testC.h
@@ -6,8 +6,8 @@
  *        https://www.remedy.nl
  */
 
-#ifndef __RIDL_TESTC_H_JGABJDJE_INCLUDED__
-#define __RIDL_TESTC_H_JGABJDJE_INCLUDED__
+#ifndef __RIDL_TESTC_H_IIICECID_INCLUDED__
+#define __RIDL_TESTC_H_IIICECID_INCLUDED__
 
 #pragma once
 
@@ -464,6 +464,6 @@ inline std::ostream& operator<< (
 
 #include /**/ "ace/post.h"
 
-#endif /* __RIDL_TESTC_H_JGABJDJE_INCLUDED__ */
+#endif /* __RIDL_TESTC_H_IIICECID_INCLUDED__ */
 
 // -*- END -*-
--- examples/valuetype/testS.h
+++ examples/valuetype/testS.h
@@ -6,8 +6,8 @@
  *        https://www.remedy.nl
  */
 
-#ifndef __RIDL_TESTS_H_GJGJBAEB_INCLUDED__
-#define __RIDL_TESTS_H_GJGJBAEB_INCLUDED__
+#ifndef __RIDL_TESTS_H_FBADFDBG_INCLUDED__
+#define __RIDL_TESTS_H_FBADFDBG_INCLUDED__
 
 #pragma once
 
@@ -300,6 +300,6 @@ namespace TAOX11_NAMESPACE {
   } // namespace CORBA
 } // namespace TAOX11_NAMESPACE
 
-#endif /* __RIDL_TESTS_H_GJGJBAEB_INCLUDED__ */
+#endif /* __RIDL_TESTS_H_FBADFDBG_INCLUDED__ */
 
 // -*- END -*-
