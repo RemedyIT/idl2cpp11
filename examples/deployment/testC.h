89c023351874b2a2e8521e6aedf687e7070638a8
Johnny Willemsen <jwillemsen@remedy.nl>
2023-08-14 17:32:03 +0200

    Add bitset constructor/swap
        * examples/array/testC.h:
        * examples/const/testC.h:
        * examples/dds/testC.h:
        * examples/dds/testS.h:
        * examples/deployment/testC.h:
        * examples/enum/testC.h:
        * examples/exceptions/testC.h:
        * examples/exceptions/testS.h:
        * examples/hello/testC.h:
        * examples/hello/testS.h:
        * examples/hello_auto/testC.h:
        * examples/hello_auto/testS.h:
        * examples/idl4/bitmask/testC.h:
        * examples/idl4/bitset/testC.h:
        * examples/idl4/map/testC.h:
        * examples/idl4/struct_inheritance/testC.h:
        * examples/local/testC.h:
        * examples/local/testS.h:
        * examples/objrefarg/testC.h:
        * examples/radartrack/testC.h:
        * examples/sequence/testC.h:
        * examples/struct/testC.h:
        * examples/union/testC.h:
        * examples/valuetype/testC.h:
        * examples/valuetype/testS.h:

--- examples/deployment/testC.h
+++ examples/deployment/testC.h
@@ -6,8 +6,8 @@
  *        https://www.remedy.nl
  */
 
-#ifndef __RIDL_TESTC_H_ECJDJICH_INCLUDED__
-#define __RIDL_TESTC_H_ECJDJICH_INCLUDED__
+#ifndef __RIDL_TESTC_H_EHHCAABA_INCLUDED__
+#define __RIDL_TESTC_H_EHHCAABA_INCLUDED__
 
 #pragma once
 
@@ -5584,6 +5584,6 @@ inline std::ostream& operator<< (std::ostream& strm, const ::Deployment::Capabil
 
 #include /**/ "ace/post.h"
 
-#endif /* __RIDL_TESTC_H_ECJDJICH_INCLUDED__ */
+#endif /* __RIDL_TESTC_H_EHHCAABA_INCLUDED__ */
 
 // -*- END -*-
